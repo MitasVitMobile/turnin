2afb882f47ef12c72be5cda2229204e4fb466248
MitasVitMobile <90557759+MitasVitMobile@users.noreply.github.com>
2022-01-28 18:20:11 +0100

    Update Hi.php

--- Hi.php
+++ Hi.php
@@ -1,12 +1,12 @@
-$file;
+//$file;
 
 $pieces = extract($file, " ", 174);
-$walls2;
+//$walls2;
 for($i =0; $i<170; $i++){
 $walls2[$i] = $pieces[$i+3];
 }
 
-$walls;
+//$walls;
 for($y =0; $y<10; $y++){
 for($x =0; $x<17; $x++){
 $walls[$y][$x]=$walls2[(y*17)+x-1];
